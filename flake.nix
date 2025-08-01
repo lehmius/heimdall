a07d10b7dd8dff17d1780e7c1911c056158b790d
lehmius <lehmius.github@clajus.eu>
2025-08-01 22:30:35 +0200

    feat: now with disko

--- flake.nix
+++ flake.nix
@@ -14,6 +14,11 @@
             url = "github:snowfallorg/lib";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+
+	disko = {
+	  url = "github:nix-community/disko";
+	  inputs.nixpkgs.follows = "nixpkgs";
+	};
     };
 
     outputs = inputs:
---
+++ systems/x86_64-linux/vm/disks.nix
@@ -0,0 +1,74 @@
+{
+  disko.devices {
+    disk = {
+      vda = {
+        type = "disk";
+	device = "/dev/vda";
+	content = {
+	  type = "gpt";
+	  partitions = {
+	    ESP = {
+	      label = "boot";
+	      name = "ESP";
+	      size = "512M";
+	      type = "EF00";
+	      content = {
+	        type = "filesystem";
+		format = "vfat";
+		mountpoint = "/boot";
+		mountOptions = [
+		  "defaults"
+		];
+	      };
+	    };
+
+	    luks = {
+	      size = "100%";
+	      label = "luks";
+	      name = "enc";
+	      extraOpenArgs = [
+	        "--allow-discards"
+		"--perf-no_read_workqueue"
+		"--perf-no_write_workqueue"
+	      ];
+	      content = {
+	        type = "btrfs";
+		extraArgs = [ "-L" "nixos" "-f"];
+		postCreateHoom = ''
+		  mount -t btrfs /dev/disk/by-label/nixos /mnt
+		  btrfs subvolume snapshot -r /mnt /mnt/root-blank
+		  umount /mnt
+		'';
+		subvolumes {
+		  "/root" = {
+		    mountpoint = "/";
+		    mountOptions = [ "subvol=root" "compress=zstd" "noatime" ];
+		  };
+		  "/home" = {
+		    mountpoint = "/home";
+		    mountOptions = [ "subvol=home" "compress=zstd" "noatime" ];
+		  };
+		  "/nix" = {
+		    mountpoint = "/nix";
+		    mountOptions = [ "subvol=nix" "compress=zstd" "noatime" ];
+		  };
+		  "/persist" = {
+		    mountpoint = "/persist";
+		    mountOptions = [ "subvol=persist" "compress=zstd" "noatime" ];
+		  };
+		  "/log" = {
+		    mountpoint = "/var/log";
+		    mountOptions = [ "subvol=log" "compress=zstd" "noatime" ];
+		  };
+		};
+	      };
+	    };
+	  };
+	};
+      };
+    };
+  };
+  
+  fileSystems."/persist".neededForBoot = true;
+  fileSystems."/var/log".neededForBoot = true;
+}
