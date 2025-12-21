b8275581366d57f5086389a480e5b20ace751341
lehmius <lehmius.github@clajus.eu>
2025-12-21 11:49:27 +0100

    Install vintagestory and update flake.lock

--- hosts/workstation/configuration.nix
+++ hosts/workstation/configuration.nix
@@ -55,6 +55,15 @@
         experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
       '';
     };
+    flatpak.enable = true;
+  };
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+      flatpak install --or-update flathub at.vintagestory.VintageStory
+    '';
   };
   services.xserver = {
     enable = true;
