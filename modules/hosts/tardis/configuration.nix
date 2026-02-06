8a23725d4bb7f59b28d00498166db86c03acffd4
lehmius <lehmius.github@clajus.eu>
2026-02-06 16:10:19 +0100

    Removed vintagestory config

--- modules/hosts/tardis/configuration.nix
+++ modules/hosts/tardis/configuration.nix
@@ -51,17 +51,6 @@
       };
     };
 
-    # Enable the GNOME Desktop Environment.
-    services.flatpak.enable = true;
-    systemd.services.flatpak-repo = {
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.flatpak ];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-        flatpak install --or-update flathub at.vintagestory.VintageStory
-      '';
-    };
-
     hardware.graphics.enable = true;
     hardware.nvidia = {
       modesetting.enable = true;
