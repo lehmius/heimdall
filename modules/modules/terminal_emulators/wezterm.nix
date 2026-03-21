02cfda1ea3267e7ca4eb19415f280e8cd69d7e75
lehmius <lehmius.github@clajus.eu>
2026-03-21 15:48:57 +0100

    feat: Expose wezterm extraConfig option to module consumer

--- modules/modules/terminal_emulators/wezterm.nix
+++ modules/modules/terminal_emulators/wezterm.nix
@@ -1,40 +1,72 @@
 {
-  flake.modules.homeManager.wezterm = {
-    programs.wezterm = {
-      enable = true;
-      colorSchemes = {
-        "Flexoki Dark" = {
-          # Flexoki Dark is a color scheme by kepano.
-          # https://github.com/kepano/flexoki/
-          ansi = [
-            "#100F0F" #Black
-            "#AF3029" # Red
-            "#66800B" # Green
-            "#AD8301" # Yellow
-            "#205EA6" # Blue
-            "#5E409D" # Purple
-            "#24837B" # Cyan
-            "#CECDC3" # White
-          ];
-          brights = [
-            "#575653" # Black
-            "#D14D41" # Red
-            "#879A39" # Green
-            "#D0A215" # Yellow
-            "#4385BE" # Blue
-            "#8B7EC8" # Purple
-            "#3AA99F" # Cyan
-            "#FFFCF0" # White
-          ];
-          foreground = "#CECDC3";
-          background = "#100F0F";
+   flake.modules.homeManager.wezterm = { config, lib, ... }:
+  let
+    cfg = config.wezterm;
+  in
+  {
+    options.wezterm = {
+      enable = lib.mkEnableOption "Enable wezterm home-manager module.";
+      extraConfig = lib.mkOption {
+        type = lib.types.string;
+        default = "";
+        example = ''
+          builtins.readFile ./dotfiles/wezterm/wezterm.lua
+          ---
+          local mylib = require 'mylib';
+          return {
+            usemylib = mylib.do_fun();
+            font = wezterm.font("JetBrains Mono"),
+            font_size = 16.0,
+            color_scheme = "Tomorrow Night",
+            hide_tab_bar_if_only_one_tab = true,
+            default_prog = { "zsh", "--login", "-c", "tmux attach -t dev || tmux new -s dev" },
+            keys = {
+              {key="n", mods="SHIFT|CTRL", action="ToggleFullScreen"},
+            }
+          }
+        '';
+        description = "Extra configuration for wezterm for which no home-manager options exist.";
+      };
+    };
 
-          cursor_bg = "#CECDC3";
-          cursor_border = "#CECDC3";
-          cursor_fg = "#100F0F";
-          selection_bg = "#282726";
-          selection_fg = "#CECDC3";
+    config = lib.mkIf cfg.enable {
+      programs.wezterm = {
+        enable = true;
+        colorSchemes = {
+          "Flexoki Dark" = {
+            # Flexoki Dark is a color scheme by kepano.
+            # https://github.com/kepano/flexoki/
+            ansi = [
+              "#100F0F" #Black
+              "#AF3029" # Red
+              "#66800B" # Green
+              "#AD8301" # Yellow
+              "#205EA6" # Blue
+              "#5E409D" # Purple
+              "#24837B" # Cyan
+              "#CECDC3" # White
+            ];
+            brights = [
+              "#575653" # Black
+              "#D14D41" # Red
+              "#879A39" # Green
+              "#D0A215" # Yellow
+              "#4385BE" # Blue
+              "#8B7EC8" # Purple
+              "#3AA99F" # Cyan
+              "#FFFCF0" # White
+            ];
+            foreground = "#CECDC3";
+            background = "#100F0F";
+
+            cursor_bg = "#CECDC3";
+            cursor_border = "#CECDC3";
+            cursor_fg = "#100F0F";
+            selection_bg = "#282726";
+            selection_fg = "#CECDC3";
+          };
         };
+        extraConfig = cfg.extraConfig;
       };
     };
   };
